e9597adfb433400047a2b5b2d85909445a9c81bd
Marco Scaletta <scalettamarco@yahoo.it>
2019-03-01 21:02:40 +0100

    deploy

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+require('find-java-home')(function(err, home){
+  if(err)return console.log(err);
+  console.log(home);
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
